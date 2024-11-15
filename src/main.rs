cb16056ae4d51ecec934b844bf6a6e128f757b5e
dalu <mou.tong@outlook.com>
2024-11-15 14:02:41 +0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,103 @@
-use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
+use fltk::{prelude::*, *};
 
 fn main() {
-    let app = app::App::default();
+    let a = app::App::default()
+        // .with_scheme(app::Scheme::Gtk)
+    ;
+    let mut win = window::Window::default().with_size(640, 480);
+    let mut col = group::Flex::default_fill().column();
+    main_panel(&mut col);
+    col.end();
+    win.resizable(&col);
+    // win.set_color(enums::Color::from_rgb(250, 250, 250));
+    win.end();
+    win.show();
+    win.size_range(600, 400, 0, 0);
+    a.run().unwrap();
+}
 
-    let mut wind = Window::new(100, 100, 400, 300, "Zechat");
-    let mut frame = Frame::new(0, 0, 400, 200, "frame");
-    let mut but = Button::new(160, 210, 80, 40, "click");
+fn buttons_panel(parent: &mut group::Flex) {
+    frame::Frame::default();
+    let w = frame::Frame::default().with_label("欢迎使用");
 
-    wind.end();
-    wind.show();
+    let mut urow = group::Flex::default().row();
+    {
+        frame::Frame::default()
+            .with_label("账号")
+            .with_align(enums::Align::Inside | enums::Align::Right);
+        let username = input::Input::default();
 
-    but.set_callback(move |_| frame.set_label("Hello World"));
+        urow.fixed(&username, 180);
+        urow.end();
+    }
 
-    app.run().unwrap();
+    let mut prow = group::Flex::default().row();
+    {
+        frame::Frame::default()
+            .with_label("密码")
+            .with_align(enums::Align::Inside | enums::Align::Right);
+        let password = input::SecretInput::default();
+
+        prow.fixed(&password, 180);
+        prow.end();
+    }
+
+    let pad = frame::Frame::default();
+
+    let mut brow = group::Flex::default().row();
+    {
+        frame::Frame::default();
+        let reg = create_button("注册");
+        let login = create_button("登录");
+
+        brow.fixed(&reg, 80);
+        brow.fixed(&login, 80);
+        brow.end();
+    }
+
+    let b = frame::Frame::default();
+
+    frame::Frame::default();
+
+    parent.fixed(&w, 60);
+    parent.fixed(&urow, 30);
+    parent.fixed(&prow, 30);
+    parent.fixed(&pad, 1);
+    parent.fixed(&brow, 30);
+    parent.fixed(&b, 30);
+}
+
+fn middle_panel(parent: &mut group::Flex) {
+    frame::Frame::default();
+
+    let mut frame = frame::Frame::default().with_label("Logo");
+    frame.set_frame(enums::FrameType::BorderBox);
+    // frame.set_color(enums::Color::from_rgb(0, 200, 0));
+    let spacer = frame::Frame::default();
+
+    let mut bp = group::Flex::default().column();
+    buttons_panel(&mut bp);
+    bp.end();
+
+    frame::Frame::default();
+
+    parent.fixed(&frame, 200);
+    parent.fixed(&spacer, 10);
+    parent.fixed(&bp, 300);
+}
+
+fn main_panel(parent: &mut group::Flex) {
+    frame::Frame::default();
+
+    let mut mp = group::Flex::default().row();
+    middle_panel(&mut mp);
+    mp.end();
+
+    frame::Frame::default();
+
+    parent.fixed(&mp, 200);
+}
+
+fn create_button(caption: &str) -> button::Button {
+    button::Button::default().with_label(caption)
 }
